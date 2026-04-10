18754f2a30e029e2f5d2478c5b0474e5a5b0f476
Artem Zakharchenko <kettanaito@gmail.com>
2026-04-10 15:06:10 +0200

    test: group server action into a fixture

--- tests/server-actions/server-actions.test.tsx
+++ tests/server-actions/server-actions.test.tsx
@@ -1,7 +1,7 @@
 import { it, expect } from 'vitest'
 import { page } from 'vitest/browser'
 import { renderAsync } from '../../src/render-async'
-import Server from './server'
+import Server from './action-from-client/server'
 
 it('dispatches a server action from the client component', async () => {
   await renderAsync(<Server />)
